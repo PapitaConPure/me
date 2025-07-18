444f182df7533493a6788411c72b965d9165cb1f
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2025-07-18 05:45:40 -0300

    Update next config to properly retrieve assets in public folder

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const repo = 'me';
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  distDir: 'docs2',
+  output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   images: {
     remotePatterns: [
       {
