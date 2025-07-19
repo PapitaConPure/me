b11d0535ddf3e9d8c0a7b3b57406067e276e038c
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2025-07-19 12:35:05 -0300

    Add 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import BackSection from "@/components/BackSection";
+
+const NotFound = () => (
+    <main>
+        <section>
+            <div className='flex justify-center'>
+                <div className='flex flex-col items-center space-y-8'>
+                    <div className='text-9xl'>🥔</div>
+                    <p>
+                        La dirección especificada no existe.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <BackSection>
+            Volver a <span className='font-semibold'>Inicio</span>
+        </BackSection>
+    </main>
+);
+
+export default NotFound;
